31d76ccd6d138cb3d8d8fc1895379b60f6c78695
willjrcom <williamjunior67@gmail.com>
2025-01-24 09:42:45 -0300

    first commit

---
+++ src/test/java/com/gazaltech/meta/MetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gazaltech.meta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
