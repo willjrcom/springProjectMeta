35543b203703f39bcbee836409b39255c2cd571a
willjrcom <williamjunior67@gmail.com>
2025-01-24 14:24:50 -0300

    feat: domain added

---
+++ src/main/java/com/gazaltech/meta/domain/Address.java
@@ -0,0 +1,17 @@
+package com.gazaltech.meta.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Address {
+    private Long id;
+    private String street;
+    private String number;
+    private String neighborhood;
+    private String city;
+    private Uf uf;
+    private String country;
+    private String zipCode;
+}
---
+++ src/main/java/com/gazaltech/meta/domain/Client.java
@@ -0,0 +1,29 @@
+package com.gazaltech.meta.domain;
+
+import java.util.List;
+
+import com.gazaltech.meta.shared.exceptions.BadRequestException;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Client {
+    private Long id;
+    private String name;
+    private String email;
+    private List<Address> addresses;
+
+    public void addAddress(Address address) {
+        if (addresses.size() >= 3) {
+            throw new BadRequestException("O cliente já possui 3 endereços cadastrados.");
+        }
+
+        addresses.add(address);
+    }
+
+    public void removeAddress(Address address) {
+        addresses.remove(address);
+    }
+}
