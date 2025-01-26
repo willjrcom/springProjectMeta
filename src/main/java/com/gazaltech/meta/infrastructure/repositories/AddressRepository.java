0d70c172f511eb57db204806355e80a95f28801c
willjrcom <williamjunior67@gmail.com>
2025-01-26 15:01:01 -0300

    refactor: repositories name

--- src/main/java/com/gazaltech/meta/infrastructure/repositories/AddressRepository.java
+++ src/main/java/com/gazaltech/meta/infrastructure/repositories/AddressRepository.java
@@ -3,9 +3,9 @@ package com.gazaltech.meta.infrastructure.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.gazaltech.meta.models.AddressModel;
+import com.gazaltech.meta.infrastructure.models.AddressModel;
 
 @Repository
-public interface AddressRepositoryImpl extends JpaRepository<AddressModel, Long> {
+public interface AddressRepository extends JpaRepository<AddressModel, Long> {
 
 }
--- src/main/java/com/gazaltech/meta/infrastructure/repositories/ClientRepository.java
+++ src/main/java/com/gazaltech/meta/infrastructure/repositories/ClientRepository.java
@@ -3,9 +3,9 @@ package com.gazaltech.meta.infrastructure.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.gazaltech.meta.models.ClientModel;
+import com.gazaltech.meta.infrastructure.models.ClientModel;
 
 @Repository
-public interface ClientRepositoryImpl extends JpaRepository<ClientModel, Long> {
+public interface ClientRepository extends JpaRepository<ClientModel, Long> {
 
 }
