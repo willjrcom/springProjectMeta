2423ecdfd37e120391bbe91bd44cc4db213e03e6
willjrcom <williamjunior67@gmail.com>
2025-01-27 15:22:14 -0300

    refactor: on create return create and patch changed to put

--- src/main/java/com/gazaltech/meta/infrastructure/controllers/AddressController.java
+++ src/main/java/com/gazaltech/meta/infrastructure/controllers/AddressController.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,7 +38,7 @@ public class AddressController {
         logger.info("Address created: {}", addressDTO);
 
         var id = addressUseCase.createAddress(addressDTO);
-        return ResponseEntity.ok(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
 
     @GetMapping("/{id}")
@@ -56,7 +57,7 @@ public class AddressController {
         return ResponseEntity.ok(addresses);
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateAddress(@PathVariable Long id,
             @RequestBody @Valid UpdateAddressDTO addressDTO) {
         logger.info("Address updated: {}", id);
--- src/main/java/com/gazaltech/meta/infrastructure/controllers/ClientController.java
+++ src/main/java/com/gazaltech/meta/infrastructure/controllers/ClientController.java
@@ -6,12 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,7 +39,7 @@ public class ClientController {
         logger.info("Client created: {}", clientDTO);
 
         var id = clientUseCase.createClient(clientDTO);
-        return ResponseEntity.ok(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
 
     @GetMapping("/{id}")
@@ -73,7 +74,7 @@ public class ClientController {
         return ResponseEntity.noContent().build();
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<String> updateClient(@PathVariable Long id, @RequestBody @Valid UpdateClientDTO clientDTO) {
         logger.info("Client updated: {}", id);
 
