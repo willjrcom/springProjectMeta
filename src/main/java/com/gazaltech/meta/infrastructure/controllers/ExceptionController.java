f3494a5d2f24c1b1778fc6583124e5080c0e800a
willjrcom <williamjunior67@gmail.com>
2025-01-27 19:13:46 -0300

    feat: exceptions added

--- src/main/java/com/gazaltech/meta/infrastructure/controllers/ExceptionController.java
+++ src/main/java/com/gazaltech/meta/infrastructure/controllers/ExceptionController.java
@@ -2,8 +2,10 @@ package com.gazaltech.meta.infrastructure.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.gazaltech.meta.shared.exceptions.BadRequestException;
 import com.gazaltech.meta.shared.exceptions.NotFoundException;
@@ -21,4 +23,18 @@ public class ExceptionController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }   
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
 }
