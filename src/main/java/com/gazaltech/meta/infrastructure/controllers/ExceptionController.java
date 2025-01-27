6a5310445f39b7c46edd3143df0c8c9d003dd4a4
willjrcom <williamjunior67@gmail.com>
2025-01-27 10:15:07 -0300

    fix: handler exceptions

--- src/main/java/com/gazaltech/meta/infrastructure/controllers/ExceptionController.java
+++ src/main/java/com/gazaltech/meta/infrastructure/controllers/ExceptionController.java
@@ -2,27 +2,23 @@ package com.gazaltech.meta.infrastructure.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.gazaltech.meta.shared.exceptions.BadRequestException;
 import com.gazaltech.meta.shared.exceptions.NotFoundException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+    public ResponseEntity<String> handleBadRequestException(BadRequestException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleMaxAddressLimitException(NotFoundException ex) {
+    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<String> handleMaxAddressLimitException(BadRequestException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
-    }
 }
