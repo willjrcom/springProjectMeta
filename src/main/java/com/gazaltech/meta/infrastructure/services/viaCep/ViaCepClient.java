6d0a86b503845204cc79bee5cc822e7d48072088
willjrcom <williamjunior67@gmail.com>
2025-01-27 10:14:15 -0300

    feat: validate feign client added

--- src/main/java/com/gazaltech/meta/infrastructure/services/viaCep/ViaCepClient.java
+++ src/main/java/com/gazaltech/meta/infrastructure/services/viaCep/ViaCepClient.java
@@ -1,11 +1,14 @@
 package com.gazaltech.meta.infrastructure.services.viaCep;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "via-cep-client", url = "https://viacep.com.br/ws")
+@Validated
 public interface ViaCepClient {
 
     @GetMapping("/{cep}/json")
-    ViaCepResponse getAddressByZipCode(@PathVariable String cep);
+    ViaCepResponse getAddressByZipCode(@PathVariable @Length(min = 8, max = 8, message = "Zip Code must be 8 characters") String cep);
 }
