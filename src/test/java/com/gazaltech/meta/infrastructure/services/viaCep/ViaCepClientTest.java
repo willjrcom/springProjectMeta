9b3aa6d265ec633f233604ca8460251f4e466b4b
willjrcom <williamjunior67@gmail.com>
2025-01-27 10:14:49 -0300

    feat: feign client test added

---
+++ src/test/java/com/gazaltech/meta/infrastructure/services/viaCep/ViaCepClientTest.java
@@ -0,0 +1,31 @@
+package com.gazaltech.meta.infrastructure.services.viaCep;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import jakarta.validation.ConstraintViolationException;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ViaCepClientTest {
+    @Autowired
+    private ViaCepClient viaCepClient;
+
+    @Test
+    void testGetAddressByZipCode_WithoutDash_Success() {
+        var cep = "01001-000";
+        var cepWithoutDash = cep.replace("-", "");
+        var response = viaCepClient.getAddressByZipCode(cepWithoutDash);
+
+        assertEquals(response.getCep(), cep);
+    }
+
+    @Test
+    void testGetAddressByZipCode_WithDash_Error() {
+        var cep = "01001-000";
+        assertThrows(ConstraintViolationException.class, () -> viaCepClient.getAddressByZipCode(cep));
+    }
+}
