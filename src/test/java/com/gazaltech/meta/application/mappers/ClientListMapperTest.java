5f9bb7dfe01f214a29a4ba522acbf2b1902ad909
willjrcom <williamjunior67@gmail.com>
2025-01-27 10:21:38 -0300

    test: client list mapper test added

--- src/test/java/com/gazaltech/meta/application/mappers/ClientListMapperTest.java
+++ src/test/java/com/gazaltech/meta/application/mappers/ClientListMapperTest.java
@@ -1,17 +1,63 @@
 package com.gazaltech.meta.application.mappers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.gazaltech.meta.domain.Client;
+import com.gazaltech.meta.infrastructure.models.ClientModel;
 
 @SpringBootTest()
 public class ClientListMapperTest {
+    private final ClientModel clientModel = ClientModel.builder()
+            .id(1L)
+            .name("William")
+            .email("will@gmail.com")
+            .cpf("436.377.998-55")
+            .build();
+
+    private final Client client = Client.builder()
+            .id(1L)
+            .name("William")
+            .email("will@gmail.com")
+            .cpf("436.377.998-55")
+            .build();
+
+    private final List<ClientModel> clientModels = List.of(clientModel, clientModel);
+    private final List<Client> clients = List.of(client, client);
+
+    @Autowired
+    private ClientListMapperImpl clientListMapper;
+
     @Test
     void testDomainsToDtos() {
+        var dtos = clientListMapper.domainsToDtos(clients);
 
+        assertEquals(clients.size(), dtos.size());
+        
+        for (int i = 0; i < clients.size(); i++) {
+            assertEquals(clients.get(i).getId(), dtos.get(i).getId());
+            assertEquals(clients.get(i).getName(), dtos.get(i).getName());
+            assertEquals(clients.get(i).getEmail(), dtos.get(i).getEmail());
+            assertEquals(clients.get(i).getCpf(), dtos.get(i).getCpf());
+        }
     }
 
     @Test
     void testModelsToDomains() {
+        var domains = clientListMapper.modelsToDomains(clientModels);
 
+        assertEquals(clientModels.size(), domains.size());
+
+        for (int i = 0; i < clientModels.size(); i++) {
+            assertEquals(clientModels.get(i).getId(), domains.get(i).getId());
+            assertEquals(clientModels.get(i).getName(), domains.get(i).getName());
+            assertEquals(clientModels.get(i).getEmail(), domains.get(i).getEmail());
+            assertEquals(clientModels.get(i).getCpf(), domains.get(i).getCpf());
+        }
     }
 }
